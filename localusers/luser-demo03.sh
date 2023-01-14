4e454a0cac8a4f5b127fc71a13a9e62088ec0dca
Oli G <oli@Seneca.fritz.box>
2023-01-14 14:18:02 +0100

    initial bash scripts

---
+++ localusers/luser-demo03.sh
@@ -0,0 +1,38 @@
+#1/bin/bash
+
+# display UID
+# display if vagrant user or not
+
+# display UID
+echo "you have UID ${UID}"
+
+# only show message, if UID != 1000
+UID_TO_TEST_FOR='1000'
+if [[ "${UID}" -ne "${UID_TO_TEST_FOR}" ]]
+then
+  echo "Your UID does not match ${UID_TO_TEST_FOR}."
+  exit 1
+else
+  echo "Your UID matches ${UID_TO_TEST_FOR}."
+fi
+
+# Display USer Name
+USER_NAME=$(id -un)		# assign result from command to variable
+
+# Test if the command succeeded
+if [[ "${?}" -ne 0 ]]		# get exit code from last command
+then
+  echo 'the id command did not execute successfully.'
+  exit 1
+fi
+echo "Your username is ${USER_NAME}"
+
+# test username matches user name to test for
+USER_NAME_TO_TEST_FOR='vagrant'
+if [[ "${USER_NAME}" = "${USER_NAME_TO_TEST_FOR}" ]]
+then
+  echo "Your username matches ${USER_NAME_TO_TEST_FOR}."
+fi
+
+exit 0
+
---
+++ localusers/luser-demo04.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# this script creates an account on the local system
+# You will be prompted for the account name and pwd
+
+# check script execution with superuser
+
+UID_TEST='0'
+if [[ "${UID}" -ne "${UID_TEST}" ]]
+then
+  echo "Your UID is not root, you need to execute this script with root"
+  exit 1
+else
+  echo "Your UID matches root: ${UID_TEST}."
+fi
+
+
+# ask for user name
+read -p 'Enter the user name to create: ' USER_NAME
+
+# ask for the real name
+read -p 'Enter the name of the person who this account is for: ' COMMENT
+
+# ask for the pwd
+read -p 'Enter pwd to use for the account: ' PWD
+
+# create user and check if succeeded, otherwise exit script
+useradd -c "${COMMENT}" -m ${USER_NAME}
+if [[ "${?}" -ne 0 ]]
+then
+  echo "useradd comand failed. script exits."
+  exit 1
+fi
+
+# set passwd and check success, otherwise exit script
+echo ${PWD} | passwd --stdin ${USER_NAME}
+if [[ "${?}" -ne 0 ]]
+then
+  echo "passwd comand failed. script exits."
+  exit 1
+fi
+
+# force pwd change on first login
+passwd -e ${USER_NAME}
+
+# display account details and host information
+echo "Login: " ${USER_NAME} "Comment: " ${COMMENT} "PWD: " ${PWD} "Created on: " $(hostname -s)
