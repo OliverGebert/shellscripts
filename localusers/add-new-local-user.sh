3412e64a5cc70204a367fc5ea28a19d08edf39ef
Oli G <oli@Seneca.fritz.box>
2023-01-22 19:22:58 +0100

    update: redirect STDERR

--- localusers/add-new-local-user.sh
+++ localusers/add-new-local-user.sh
@@ -5,45 +5,54 @@
 
 # check script execution with superuser
 
+# echo start of script and date as first lines for STD_ERR
+echo -e "\tScript started on: $(date) " >&2
+
+# test whether UID shows root priviledges, else exit 1
 UID_TEST='0'
 if [[ "${UID}" -ne "${UID_TEST}" ]]
 then
-  echo "Your UID is not root, you need to execute this script with root"
-  exit 1
-else
-  echo "Your UID matches root: ${UID_TEST}."
+    echo "You need to be root"
+    echo -e "\tYour UID is ${UID}, not root, you need to execute this script with root. Script exits" >&2
+    exit 1
 fi
 
+# test whether 2 parameters are entered for USER_NAME and COMMENT, else exit 1
 PARAM_TEST='2'
 if [[ "${#}" -ge "${PARAM_TEST}" ]]
 then
-  USER_NAME=${1}
-  COMMENT=${2}
+    USER_NAME=${1}
+    COMMENT=${2}
 else
-  echo "Usage: ${0} USER_NAME COMMENT"
-  exit 1
+    echo "Missing arguments: USER_NAME COMMENT. Script exits"
+    echo -e "\tMissing arguments, usage: ${0} USER_NAME COMMENT. Script exits." >&2
+    exit 1
 fi
 
+# generate inital pasword with 10 chars
 PASSWORD=$(echo "${RANDOM}${RANDOM}" | base64 | head -c10)
 
 # create user and check if succeeded, otherwise exit script
-useradd -c "${COMMENT}" -m ${USER_NAME}
+useradd -c "${COMMENT}" -m ${USER_NAME} > /dev/null
 if [[ "${?}" -ne 0 ]]
 then
-  echo "useradd comand failed. script exits."
-  exit 1
+    echo "useradd comand failed. script exits."
+    echo -e "\tuseradd -c ${COMMENT} -m ${USER_NAME} failed. Script exits" >&2
+    exit 1
 fi
 
 # set passwd and check success, otherwise exit script
-echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} > /dev/null
 if [[ "${?}" -ne 0 ]]
 then
-  echo "passwd comand failed. script exits."
-  exit 1
+    echo "passwd command failed. script exits."
+    echo -e "\tpasswd command failed. script exits." >&2
+    exit 1
 fi
 
 # force pwd change on first login
-passwd -e ${USER_NAME}
+passwd -e ${USER_NAME} > /dev/null
 
-# display account details and host information
+# display account details and host information, exit 0 successfully
 echo -e "\nLogin: " ${USER_NAME} "\nComment: " ${COMMENT} "\nPASSWORD: " ${PASSWORD} "\nCreated on: " $(hostname -s) "\n"
+exit 0
