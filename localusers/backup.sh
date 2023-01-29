652762a3c570f6a1a806c4da346b5b0cad150b8b
Oli G <oli@Seneca.fritz.box>
2023-01-29 18:48:41 +0100

    change: backup.sh completed

--- localusers/backup.sh
+++ localusers/backup.sh
@@ -3,8 +3,6 @@
 # use functions to capsulate code blocks. log level is defined on input parameter for script. If value other than 1|2 is given only put MESSAGE to logger
 
 log() {
-    local VERBOSE="${1}"
-    shift # shift the parameter list one to the left
     local MESSAGE="${@}"
     case ${VERBOSE} in
         1) echo "${MESSAGE}" ;;
@@ -13,8 +11,36 @@ log() {
     esac
 }
 
-VERBOSITY="${1}"
-log "${VERBOSITY}" 'Hello'
-log "${VERBOSITY}" 'to this new function'
+backup() {
+    # make sure file exists
+    if [[ -f "${FILE}" ]]
+    then
+        local BACKUPFILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+        log "copying ${FILE} to ${BACKUPFILE}"
+        # the exit status will be the exit status of cp as it is the last command
+        cp -p ${FILE} ${BACKUPFILE}
+    else
+        log "copying ${FILE} to ${BACKUPFILE} failed."
+        return 1
+    fi
+}
 
+if [[ "${#}" -ge 2 ]]
+then
+    readonly VERBOSE="${1}"
+    readonly FILE="${2}"
+    backup
+    if [[ "${?}" -eq 0 ]]
+    then
+        log "backup succeeded"
+    else
+        log "backup failed."
+        exit 1
+    fi
+else
+    readonly VERBOSE='1'
+    log "not enough parameters"
+    exit 1
+fi
+exit 0
 
