3b5fe9cc7e922381aa3afcb5b28d45f921de236a
Oli G <oli@Seneca.fritz.box>
2023-02-04 22:46:45 +0100

    add:luser-demo12.sh to delete local user

---
+++ localusers/luser-demo12.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this script deletes a user
+
+# run root
+
+if [[ "${UID}" -ne 0 ]]
+then
+  echo "please run in root" >&2
+  exit 1
+fi
+
+# assume the first argument is the user t delete
+
+USER="${1}"
+
+userdel -r ${USER}
+
+# make sure the user got deleted
+
+if [[ "${?}" -ne 0 ]]
+then
+  echo "the account ${USER} was not deleted" >&2
+  exit 1
+fi
+
+echo "the account ${USER} was deleted, including the home dir"
+
+exit 0
+
